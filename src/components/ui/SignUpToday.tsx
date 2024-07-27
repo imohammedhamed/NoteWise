8901470a16841a10d3c32c367e3f9efa2342b713
imohammedhamed <mohammedhamed6726@gmail.com>
2024-07-27 18:30:58 +0300

    fix The Mobile view

--- src/components/ui/SignUpToday.tsx
+++ src/components/ui/SignUpToday.tsx
@@ -5,7 +5,7 @@ export default function SignUpToday() {
   return (
     <section className=" w-full py-10 my-10 border-t border-b border-LightPurple">
         <MaxWContainer className=" flex justify-center items-center flex-col gap-10">
-            <h3 className=" text-2xl text-center bg-gradient-to-t from-Purple700 to-LightPurple bg-clip-text  font-semibold ">Ready to Experience the Future of Note-Taking? <br /> Sign Up Today and Start Your Free Trial</h3>
+            <h3 className=" lg:text-2xl text-lg text-center bg-gradient-to-t from-Purple700 to-LightPurple bg-clip-text  font-semibold ">Ready to Experience the Future of Note-Taking? <br /> Sign Up Today and Start Your Free Trial</h3>
             <Button><Link href="/login" className="lg:text-base text-sm px-10">Sign Up</Link></Button>
         </MaxWContainer>
     </section>
