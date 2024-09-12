d1ce70dd2ad376f2cbda73a67924ce98a94e9067
imohammedhamed <mohammedhamed6726@gmail.com>
2024-09-12 13:49:26 +0300

    Landing Page Fix the text in the Banner Component

--- src/components/landingPage-components/TopBanner.tsx
+++ src/components/landingPage-components/TopBanner.tsx
@@ -1,7 +1,7 @@
 
 export default function TopBanner() {
   return (
-    <p className=" w-full p-1.5 flex justify-center items-center bg-Purple700 lg:text-base text-sm font-semibold text-Purple50">
+    <p className=" w-full p-1.5 flex justify-center items-center bg-Purple700 lg:text-base text-xm text-center font-semibold text-Purple50">
         {` ✨! We're integrating a Notion-style WYSIWYG rich text editor, powered by Novel.`} 
     </p>
   )
