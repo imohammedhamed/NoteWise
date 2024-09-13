c62334478975720e878706cec88656c69bc10248
imohammedhamed <mohammedhamed6726@gmail.com>
2024-09-13 21:32:45 +0300

    flex few things in The Landing Page

--- src/components/landingPage-components/FeaturesSection.tsx
+++ src/components/landingPage-components/FeaturesSection.tsx
@@ -1,24 +1,24 @@
 import MaxWContainer from "../ui/MaxWContainer"
 import SectionHeading from "./SectionHeading"
-import { aboutCard } from "../../lib/data"
+import { features } from "@/lib/data"
 import Section from "../ui/Section"
 import Image from "next/image"
-export default function AboutSection() {
+export default function FeaturesSection() {
   return (
-        <Section sectionId="about">
+        <Section sectionId="features">
         <MaxWContainer>
             <SectionHeading
-            SectionTitle = "About Us"
+            SectionTitle = "Features"
             SectionSubTitle="Dive deeper into how our AI-powered note-taking platform can revolutionize your productivity and idea management."
             />
             <div className=" container grid lg:grid-cols-2 grid-cols-1 gap-5">
                 {
-                    aboutCard.map(item =>{
+                    features.map(item =>{
                         return(
                             <div key={item.id} className=" w-full flex justify-center items-center">
                                 <div className=" space-y-3 flex flex-col justify-center items-start p-6 bg-Purple50 border border-solid border-LightPurple rounded-xl w-full h-max lg:w-[800px] lg:h-36">
                                     <span className=" flex justify-center items-center gap-3">
-                                    <Image src={item.imgSrc} alt="about img" width={30} height={30} className=" mb-1.5"/>
+                                    <Image src={item.imgSrc} alt="about img" width={35} height={35} className=" mb-1.5"/>
                                     <h3 className=" pb-2 text-lg text-DarkPurple font-bold ">{item.Title}</h3>
                                     </span>
                                     <p className=" text-base text-DarkPurple/50 font-bold">{item.Body}</p>
--- src/components/landingPage-components/HeroSection.tsx
+++ src/components/landingPage-components/HeroSection.tsx
@@ -9,17 +9,18 @@ import BlurEffect from "../ui/BlurEffect"
 export default function HeroSection() {
   return (
     <motion.section
-        initial={{y:10,opacity:0}}
+        initial={{y:50,opacity:0}}
         whileInView={{y:0,opacity:1}}
-        transition={{ease:"linear",delay:0.2}}
-        className=" mt-44 flex justify-center items-center w-full">
+        transition={{ease:"easeInOut",duration:0.7,delay:0.2}}
+        viewport={{ once: true, amount: 0.5 }}
+        className=" min-h-[80vh] mt-48 flex justify-center items-center w-full">
         <MaxWContainer className="*:text-center relative px-4">
         <h1 className=" lg:text-5xl text-2xl font-semibold pb-5">Take Notes to the Next Level with <br /> <span className=" border-b-[5px] border-Purple700">AI-Powered</span> Conversations</h1>
         <p className=" lg:text-xl text-base font-medium">Interact with Your Notes Like Never Before. <br /> Organize, Discuss, and Enhance Your Ideas Seamlessly.</p>
         <BlurEffect className="top-10 left-10 w-32 h-[20rem] lg:h-[50rem]"/>
         <span className=" w-full flex justify-center items-center pt-7  pb-20 *:text-base *:font-semibold">
         <Button className="mx-2"><Link href="/signup" className="lg:text-base text-sm font-medium">Get Started for Free</Link></Button>
-        <Button className="mx-2" variant="ghost"><Link href="/#about" className="lg:text-base text-sm font-medium">Learn More</Link></Button>
+        <Button className="mx-2" variant="ghost"><Link href="/#features" className="lg:text-base text-sm font-medium">Learn More</Link></Button>
         </span>
         <Image src={heroImg} alt="hero img" priority quality={100} className=" lg:p-5 md:p-5 p-2.5 rounded-xl bg-DarkPurple/10 backdrop-blur-xl "/>
         </MaxWContainer>
