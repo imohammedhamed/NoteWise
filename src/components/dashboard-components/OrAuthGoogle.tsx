9ba5e6b745b458b2aecf11d339d98cc455a3beb3
imohammedhamed <mohammedhamed6726@gmail.com>
2024-11-26 14:05:27 +0200

    fix The Auth UI pages

---
+++ src/components/dashboard-components/OrAuthGoogle.tsx
@@ -0,0 +1,5 @@
+import { MdOutlineHorizontalRule } from "react-icons/md";
+
+export default function OrAuthGoogle() {
+  return <p className=' text-brand_primary/50 text-sm font-extrabold w-full text-center py-1 flex justify-center items-center gap-1'><MdOutlineHorizontalRule/>or<MdOutlineHorizontalRule/></p>
+}
